34aa04645f1ede3f991c7417ba02da185a40c1f7
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-09-23 22:59:21 +0200

    remove completable future

--- service/city/repository/src/test/java/com/sebczu/poc/transaction/city/repository/ReadCommitedTransactionCityRepositoryTest.java
+++ service/city/repository/src/test/java/com/sebczu/poc/transaction/city/repository/ReadCommitedTransactionCityRepositoryTest.java
@@ -7,9 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
@@ -23,11 +20,10 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
     // T2 ---(startTransaction)---(addPopulation)---(saveInDB[1] | endTransaction)
     @Test
     @DisplayName("TransactionReadCommited(start transaction) -> SecondTransaction(add population) -> TransactionReadCommited(read commited population)")
-    void startTransactionT1_addPopulationT2_readPopulationT1() throws ExecutionException, InterruptedException {
+    void startTransactionT1_addPopulationT2_readPopulationT1() {
         CityEntity city = repository.save(CityFactory.create());
 
-        CompletableFuture<CityEntity> readCommited = CompletableFuture.supplyAsync(() -> service.wait_readAndAddPopulation(city.getId()));
-        readCommited.get();
+        service.wait_readAndAddPopulation(city.getId());
 
         assertThat(repository.findAll())
                 .hasSize(1)
@@ -39,11 +35,10 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
     // T2 ---(startTransaction)----------------------------(addPopulation)---(saveInDB[1] | endTransaction)
     @Test
     @DisplayName("TransactionReadCommited(read population) -> SecondTransaction(add population) -> TransactionReadCommited(add population)")
-    void readPopulationT1_addPopulationT2_addPopulationT1() throws ExecutionException, InterruptedException {
+    void readPopulationT1_addPopulationT2_addPopulationT1() {
         CityEntity city = repository.save(CityFactory.create());
 
-        CompletableFuture<CityEntity> readCommited = CompletableFuture.supplyAsync(() -> service.readPopulation_wait_addPopulation(city.getId()));
-        readCommited.get();
+        service.readPopulation_wait_addPopulation(city.getId());
 
         assertThat(repository.findAll())
                 .hasSize(1)
@@ -56,11 +51,10 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
     @Test
     @DisplayName("TransactionReadCommited(read population) -> SecondTransaction(add population) -> TransactionReadCommited(read and add population)")
     // default entity manager cache entites in L1 cache (cache by transaction)
-    void readPopulationT1_addPopulationT2_readAndAddPopulationT1() throws ExecutionException, InterruptedException {
+    void readPopulationT1_addPopulationT2_readAndAddPopulationT1() {
         CityEntity city = repository.save(CityFactory.create());
 
-        CompletableFuture<CityEntity> readCommited = CompletableFuture.supplyAsync(() -> service.readPopulation_wait_readAndAddPopulation(city.getId()));
-        readCommited.get();
+        service.readPopulation_wait_readAndAddPopulation(city.getId());
 
         assertThat(repository.findAll())
                 .hasSize(1)
@@ -72,11 +66,10 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
     // T2 ---(startTransaction)----------------------------(addPopulation)---(saveInDB[1] | endTransaction)
     @Test
     @DisplayName("TransactionReadCommited(read population) -> SecondTransaction(add population) -> TransactionReadCommited(clean cache and read population)")
-    void readPopulationT1_addPopulationT2_cleanCacheAndReadPopulationT1() throws ExecutionException, InterruptedException {
+    void readPopulationT1_addPopulationT2_cleanCacheAndReadPopulationT1() {
         CityEntity city = repository.save(CityFactory.create());
 
-        CompletableFuture<CityEntity> readCommited = CompletableFuture.supplyAsync(() -> service.readPopulation_wait_cleanCacheAndReadPopulation(city.getId()));
-        readCommited.get();
+        service.readPopulation_wait_cleanCacheAndReadPopulation(city.getId());
 
         assertThat(repository.findAll())
                 .hasSize(1)
@@ -88,11 +81,10 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
     // T2 ---(startTransaction)---(addCity)---(saveInDB[2] | endTransaction)
     @Test
     @DisplayName("TransactionReadCommited(start transaction) -> SecondTransaction(add city) -> TransactionReadCommited(read commited city)")
-    void startTransactionT1_addCityT2_readCitiesT1() throws ExecutionException, InterruptedException {
+    void startTransactionT1_addCityT2_readCitiesT1() {
         repository.save(CityFactory.create());
 
-        CompletableFuture<Integer> readCommited = CompletableFuture.supplyAsync(() -> service.wait_readCities());
-        int size = readCommited.get();
+        int size = service.wait_readCities();
 
         assertThat(size).isEqualTo(2);
         assertThat(repository.findAll())
@@ -103,11 +95,10 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
     // T2 ---(startTransaction)---------------------(addCity)---(saveInDB[2] | endTransaction)
     @Test
     @DisplayName("TransactionReadCommited(start transaction) -> SecondTransaction(add city) -> TransactionReadCommited(read commited city)")
-    void readCitiesT1_addCityT2_readCitiesT1() throws ExecutionException, InterruptedException {
+    void readCitiesT1_addCityT2_readCitiesT1() {
         repository.save(CityFactory.create());
 
-        CompletableFuture<Integer> readCommited = CompletableFuture.supplyAsync(() -> service.readCities_wait_readCities());
-        int size = readCommited.get();
+        int size = service.readCities_wait_readCities();
 
         assertThat(size).isEqualTo(2);
         assertThat(repository.findAll())
