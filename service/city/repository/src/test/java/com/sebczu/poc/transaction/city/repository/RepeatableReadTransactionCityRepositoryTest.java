9b469fcc6fcd9a65dd4854b103f59f0aaec0af1b
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-09-24 22:07:52 +0200

    add repeatable read transaction tests

--- service/city/repository/src/test/java/com/sebczu/poc/transaction/city/repository/RepeatableReadTransactionCityRepositoryTest.java
+++ service/city/repository/src/test/java/com/sebczu/poc/transaction/city/repository/RepeatableReadTransactionCityRepositoryTest.java
@@ -2,16 +2,18 @@ package com.sebczu.poc.transaction.city.repository;
 
 import com.sebczu.poc.transaction.city.repository.entity.CityEntity;
 import com.sebczu.poc.transaction.city.repository.factory.CityFactory;
-import com.sebczu.poc.transaction.city.repository.service.ReadCommitedCityService;
+import com.sebczu.poc.transaction.city.repository.service.RepeatableReadCityService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.CannotAcquireLockException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
-class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
+class RepeatableReadTransactionCityRepositoryTest extends CityRepositoryTest {
 
     @Autowired
-    private ReadCommitedCityService service;
+    private RepeatableReadCityService service;
 
     //[X] - actual value in transaction
 
@@ -29,13 +31,16 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
                 .extracting(CityEntity::getPopulation).isEqualTo(2);
     }
 
-    // T1 ---(startTransaction)---(readPopulation[0])---------------------------------------------------------(addPopulation)---(saveInDB[1] | endTransaction)
+    // T1 ---(startTransaction)---(readPopulation[0])---------------------------------------------------------(addPopulation)---(rollbackTransaction)
     // T2 ---(startTransaction)----------------------------(addPopulation)---(saveInDB[1] | endTransaction)
     @Test
     void readPopulationT1_addPopulationT2_addPopulationT1() {
         CityEntity city = repository.save(CityFactory.create());
 
-        service.readPopulation_wait_addPopulation(city.getId());
+        Throwable thrown = catchThrowable(() -> service.readPopulation_wait_addPopulation(city.getId()));
+
+        assertThat(thrown)
+                .isInstanceOf(CannotAcquireLockException.class);
 
         assertThat(repository.findAll())
                 .hasSize(1)
@@ -43,14 +48,16 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
                 .extracting(CityEntity::getPopulation).isEqualTo(1);
     }
 
-    // T1 ---(startTransaction)---(readPopulation[0])--------------------------------------------------------(readPopulationFromCache[0] | addPopulation)---(saveInDB[1] | endTransaction)
+    // T1 ---(startTransaction)---(readPopulation[0])--------------------------------------------------------(readPopulationFromCache[0] | addPopulation)---(rollbackTransaction)
     // T2 ---(startTransaction)----------------------------(addPopulation)---(saveInDB[1] | endTransaction)
     @Test
-    // default entity manager cache entites in L1 cache (cache by transaction)
     void readPopulationT1_addPopulationT2_readAndAddPopulationT1() {
         CityEntity city = repository.save(CityFactory.create());
 
-        service.readPopulation_wait_readAndAddPopulation(city.getId());
+        Throwable thrown = catchThrowable(() -> service.readPopulation_wait_readAndAddPopulation(city.getId()));
+
+        assertThat(thrown)
+                .isInstanceOf(CannotAcquireLockException.class);
 
         assertThat(repository.findAll())
                 .hasSize(1)
@@ -58,18 +65,21 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
                 .extracting(CityEntity::getPopulation).isEqualTo(1);
     }
 
-    // T1 ---(startTransaction)---(readPopulation[0])--------------------------------------------------------(cleanCacheAndReadPopulation[1] | addPopulation)---(saveInDB[2] | endTransaction)
+    // T1 ---(startTransaction)---(readPopulation[0])--------------------------------------------------------(cleanCacheAndReadPopulation[1] | addPopulation)---(rollbackTransaction)
     // T2 ---(startTransaction)----------------------------(addPopulation)---(saveInDB[1] | endTransaction)
     @Test
     void readPopulationT1_addPopulationT2_cleanCacheAndReadPopulationT1() {
         CityEntity city = repository.save(CityFactory.create());
 
-        service.readPopulation_wait_cleanCacheAndReadPopulation(city.getId());
+        Throwable thrown = catchThrowable(() -> service.readPopulation_wait_cleanCacheAndReadPopulation(city.getId()));
+
+        assertThat(thrown)
+                .isInstanceOf(CannotAcquireLockException.class);
 
         assertThat(repository.findAll())
                 .hasSize(1)
                 .element(0)
-                .extracting(CityEntity::getPopulation).isEqualTo(2);
+                .extracting(CityEntity::getPopulation).isEqualTo(1);
     }
 
     // T1 ---(startTransaction)------------------------------------------------(readCities[2] | endTransaction)
@@ -85,7 +95,7 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
                 .hasSize(2);
     }
 
-    // T1 ---(startTransaction)---(readCities[1])-----------------------------------------------(readCities[2])---(endTransaction)
+    // T1 ---(startTransaction)---(readCities[1])-----------------------------------------------(readCities[1])---(endTransaction)
     // T2 ---(startTransaction)---------------------(addCity)---(saveInDB[2] | endTransaction)
     @Test
     void readCitiesT1_addCityT2_readCitiesT1() {
@@ -93,7 +103,7 @@ class ReadCommitedTransactionCityRepositoryTest extends CityRepositoryTest {
 
         int size = service.readCities_wait_readCities();
 
-        assertThat(size).isEqualTo(2);
+        assertThat(size).isEqualTo(1);
         assertThat(repository.findAll())
                 .hasSize(2);
     }
---
+++ service/city/repository/src/test/java/com/sebczu/poc/transaction/city/repository/service/RepeatableReadCityService.java
@@ -0,0 +1,94 @@
+package com.sebczu.poc.transaction.city.repository.service;
+
+import com.sebczu.poc.transaction.city.repository.CityRepository;
+import com.sebczu.poc.transaction.city.repository.entity.CityEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static com.sebczu.poc.transaction.city.repository.service.BasicCityService.POPULATION_TO_ADD;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RepeatableReadCityService {
+
+    private final CityRepository repository;
+    private final EntityManager entityManager;
+    private final ModifierCityService modifierService;
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public CityEntity wait_readAndAddPopulation(int id) {
+        modifierService.addPopulation(id);
+
+        CityEntity city = repository.getOne(id);
+        log.info("readCommited population: " + city.getPopulation());
+        city.setPopulation(city.getPopulation() + POPULATION_TO_ADD);
+        return repository.save(city);
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public CityEntity readPopulation_wait_addPopulation(int id) {
+        CityEntity city = repository.getOne(id);
+        Integer population = city.getPopulation();
+        log.info("readCommited population: " + population);
+
+        modifierService.addPopulation(id);
+
+        city.setPopulation(population + POPULATION_TO_ADD);
+        return repository.save(city);
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public CityEntity readPopulation_wait_readAndAddPopulation(int id) {
+        CityEntity city = repository.getOne(id);
+        log.info("readCommited population: " + city.getPopulation());
+
+        modifierService.addPopulation(id);
+
+        city = repository.getOne(id);
+        log.info("readCommited population: " + city.getPopulation());
+        city.setPopulation(city.getPopulation() + POPULATION_TO_ADD);
+        return repository.save(city);
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public CityEntity readPopulation_wait_cleanCacheAndReadPopulation(int id) {
+        CityEntity city = repository.getOne(id);
+        log.info("readCommited population: " + city.getPopulation());
+
+        modifierService.addPopulation(id);
+
+        entityManager.clear();
+        city = repository.getOne(id);
+        log.info("readCommited population: " + city.getPopulation());
+        city.setPopulation(city.getPopulation() + POPULATION_TO_ADD);
+        return repository.save(city);
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public Integer wait_readCities() {
+        modifierService.addCity();
+
+        int size = repository.findAllByName("Cracow").size();
+        log.info("readCommited citites: " + size);
+        return size;
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public Integer readCities_wait_readCities() {
+        int size = repository.findAllByName("Cracow").size();
+        log.info("readCommited citites: " + size);
+
+        modifierService.addCity();
+
+        size = repository.findAllByName("Cracow").size();
+        log.info("readCommited citites: " + size);
+        return size;
+    }
+
+}
